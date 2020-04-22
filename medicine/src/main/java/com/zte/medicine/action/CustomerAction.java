e79f78ccd35cfba2de1c4b4868c7b86ef6cc21ee
qwerqqwewq <2725543850@qq.com>
2020-04-22 19:49:39 +0800

    2020.3.23

--- medicine/src/main/java/com/zte/medicine/action/CustomerAction.java
+++ medicine/src/main/java/com/zte/medicine/action/CustomerAction.java
@@ -1,8 +1,18 @@
 package com.zte.medicine.action;
 
+import com.google.gson.Gson;
 import com.opensymphony.xwork2.ActionSupport;
+import com.zte.medicine.entity.Customer;
+import com.zte.medicine.form.CustomerForm;
+import com.zte.medicine.service.CustomerService;
+import org.apache.struts.action.ActionForm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author:helloboy
@@ -12,4 +22,79 @@ import org.springframework.stereotype.Controller;
 @Controller("CustomerAction")
 @Scope("prototype")
 public class CustomerAction extends ActionSupport {
+
+    @Autowired
+    private CustomerService customerService;
+
+    /**
+     * 添加顾客信息
+     * @param form
+     * @param request
+     * @return
+     * @throws Exception
+     */
+    public String addCustomer(ActionForm form, HttpServletRequest request)throws Exception{
+
+        Map map = new HashMap(50);
+        Gson gson =new Gson();
+
+        CustomerForm customerForm = (CustomerForm)form;
+        Customer customer = new Customer();
+
+        if (customerService.findCustomerByName(customerForm.getCustomerName())!=null) {
+            customer.setCity(customerForm.getCity());
+            customer.setCustomerCode(customerForm.getCustomerCode());
+            customer.setCustomerName(customerForm.getCustomerName());
+            customer.setCustomerTel(customerForm.getCustomerTel());
+
+            try {
+                customerService.addCustomer(customer);
+                map.put("msg", "添加成功");
+            } catch (Exception e) {
+                map.put("msg", "插入失败");
+            }
+        }else {
+            map.put("msg", "顾客信息已存在");
+        }
+        return gson.toJson(map);
+    }
+
+    /**
+     * 修改客户信息
+     * @param form
+     * @param request
+     * @return
+     * @throws Exception
+     */
+    public String updateCustomer(ActionForm form,HttpServletRequest request)throws Exception{
+        Map map = new HashMap(50);
+        Gson gson =new Gson();
+
+        CustomerForm customerForm = (CustomerForm)form;
+        Customer customer = new Customer();
+
+        customer.setCity(customerForm.getCity());
+        customer.setCustomerCode(customerForm.getCustomerCode());
+        customer.setCustomerName(customerForm.getCustomerName());
+        customer.setCustomerTel(customerForm.getCustomerTel());
+
+        try {
+            customerService.modifyCustomer(customer);
+            map.put("msg", "添加成功");
+        } catch (Exception e) {
+            map.put("msg", "插入失败");
+        }
+
+        return gson.toJson(map);
+    }
+
+    /**
+     * 根据姓名显示客户信息
+     * @param form
+     */
+    public void viewByName(ActionForm form){
+        CustomerForm customerForm = (CustomerForm)form;
+        customerService.findCustomerByName(customerForm.getCustomerName());
+    }
+
 }
--- medicine/src/main/java/com/zte/medicine/action/FirmAction.java
+++ medicine/src/main/java/com/zte/medicine/action/FirmAction.java
@@ -1,9 +1,95 @@
 package com.zte.medicine.action;
 
+import com.google.gson.Gson;
+import com.zte.medicine.entity.Firm;
+import com.zte.medicine.form.FirmForm;
+import com.zte.medicine.service.FirmService;
+import org.apache.struts.action.ActionForm;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Author:helloboy
  * Date:2020-02-28 12:53
  * Description:<描述>
  */
 public class FirmAction {
+    @Autowired
+    private FirmService firmService;
+
+    /**
+     * 添加供应商信息
+     * @param form
+     * @param request
+     * @return
+     * @throws Exception
+     */
+    public String addCustomer(ActionForm form, HttpServletRequest request)throws Exception{
+
+        Map map = new HashMap(50);
+        Gson gson =new Gson();
+
+        FirmForm firmForm = (FirmForm) form;
+        Firm firm = new Firm();
+
+        if (firmService.findFirmByName(firmForm.getFirmName())!=null) {
+            firm.setCity(firmForm.getCity());
+            firm.setFirmCode(firmForm.getFirmCode());
+            firm.setFirmName(firmForm.getFirmName());
+            firm.setLinkTel(firmForm.getLinkTel());
+            firm.setLink(firmForm.getLink());
+
+            try {
+                firmService.addFirm(firm);
+                map.put("msg", "添加成功");
+            } catch (Exception e) {
+                map.put("msg", "插入失败");
+            }
+        }else {
+            map.put("msg", "供应商信息已存在");
+        }
+        return gson.toJson(map);
+    }
+
+    /**
+     * 修改供应商信息
+     * @param form
+     * @param request
+     * @return
+     * @throws Exception
+     */
+    public String updateCustomer(ActionForm form, HttpServletRequest request)throws Exception{
+        Map map = new HashMap(50);
+        Gson gson =new Gson();
+
+        FirmForm firmForm = (FirmForm) form;
+        Firm firm = new Firm();
+
+        firm.setCity(firmForm.getCity());
+        firm.setFirmCode(firmForm.getFirmCode());
+        firm.setFirmName(firmForm.getFirmName());
+        firm.setLinkTel(firmForm.getLinkTel());
+        firm.setLink(firmForm.getLink());
+
+        try {
+            firmService.modifyFirm(firm);
+            map.put("msg", "添加成功");
+        } catch (Exception e) {
+            map.put("msg", "插入失败");
+        }
+
+        return gson.toJson(map);
+    }
+
+    /**
+     * 根据姓名显示供应商信息
+     * @param form
+     */
+    public void viewByName(ActionForm form){
+        FirmForm firmForm = (FirmForm)form;
+        firmService.findFirmByName(firmForm.getFirmName());
+    }
 }
